c984be7e63267a705241a6a4f3ff8a59ab9a252c
Pablokcrs <pablokcrs@gmail.com>
2023-08-15 21:28:06 -0300

    Realizado de Ejercicios complementarios

--- TP 2 Python.py
+++ TP 2 Python.py
@@ -1,1 +1,56 @@
-var1
+# Ejercicios del 1 al 4
+numero1 = 10
+numero2 = 9.1
+suma = numero1 + numero2
+resta = numero1 - numero2
+multi = numero1 * numero2
+divi = numero1/numero2
+print("La suma entre los números es de",suma)
+print("La resta entre los números es de",resta)
+print("La multiplicación entre los números es de",multi)
+print("La división entre los números es de",divi)
+
+#Ejercicio 5
+nombre = "Pablo"
+
+#ejercicio 6,7 y 8
+precio = 100.0
+descuento = 0.20
+precio_final = precio - (precio*descuento)
+print("El precio final es de ", precio_final)
+
+#Ejercicio 9 y 10
+cadena = "Crea una variable llamada cadena."
+longitud = len(cadena)
+print(longitud)
+
+#Ejercicio 11
+precio = int(100.2)
+print(precio)
+
+#Ejercicio 12
+nombre = "Pablo "
+apellido = "Cáceres"
+nombre_completo = nombre + apellido
+print(nombre_completo)
+
+#Ejercicio 13
+edad = 20 
+edad = edad + 5
+print(edad)
+edad = edad - 10
+print(edad)
+
+#Ejercicio 14
+altura = 1.74
+altura = altura * 4
+altura = altura/3
+print(altura)
+
+#Ejercicio 15 no me funciona la función lowe
+var1 = "PABLO"
+var1 = lower()
+print(var1)
+
+#Ejercicio 16 lo mismo que el anterior
+
