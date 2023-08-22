1995610006ff75a3ed76504c96c70b5dfaee74f6
Pablokcrs <pablokcrs@gmail.com>
2023-08-22 11:08:45 -0300

    Tercera semana, el ejercicio complementario

---
+++ Ejercicio en clase - semana 3 - Condicionales.py
@@ -0,0 +1,53 @@
+print("Ingrese la fecha actual:")
+lista1 =[]
+dia = input("día de la semana: ")
+dia = dia.lower()
+if(dia != "lunes" and dia != "martes" and dia != "miercoles" and dia != "jueves" and dia != "viernes"):
+    dia=str("Se produjo un error.")
+
+
+
+dd = int(input("Número de la semana: "))
+if(dd > 32 or dd < 0):
+    dd=str("Se produjo un error.")
+
+
+mm = int(input("Número de mes: "))
+if(mm>12 or mm<0):
+    mm=str("Se produjo un error.")
+
+if(dia == "Se produjo un error." or dd == "Se produjo un error." or mm == "Se produjo un error."):
+    print("Se produjo un error, se ingreso la fecha incorrecta")
+else:
+    print("[", dia,", ", dd, "/", mm, "]")
+
+
+
+if(dia == "lunes" or dia == "martes" or dia == "miercoles"):
+    exa = print("Se tomaron examenes?")
+    exa = exa.lower()
+    if(exa == "si"): 
+        aprob = int(input("Ingrese la cantidad de aprobados: "))
+        desaprob = int(input("ingrese la cantidad de desaprobados: "))
+        tt =  aprob + desaprob 
+        aprob1 = (aprob*100)/tt
+        desaprob1 = (desaprob*100)/tt
+        if(aprob1 == 0):
+            print("Desaprobó toda la clase.")
+            if(desaprob1 == 0 ):
+                print("Aprobó toda la clase.")
+            else:
+            print("Aprobó el ", aprob1, "% y desaprobó el ", desaprob1, "% de la clase")
+
+if(dia == "jueves"):
+    porcent = int(input("Ingresar el porcentaje de asistencia: "))
+    if( porcent > 50):
+        print("Asistió la mayoría.")
+    else:
+        print("No asistió la mayoría.")
+
+if(dia == "viernes"):
+
+
+
+
