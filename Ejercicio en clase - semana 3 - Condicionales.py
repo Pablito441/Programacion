375369dc73eb7f83e6cfef349932ae6305e7f3b7
Pablokcrs <pablokcrs@gmail.com>
2023-08-23 00:54:23 -0300

    Terminé el ejercicio que no terminé en clases

--- Ejercicio en clase - semana 3 - Condicionales.py
+++ Ejercicio en clase - semana 3 - Condicionales.py
@@ -1,18 +1,19 @@
-print("Ingrese la fecha actual:")
-lista1 =[]
-dia = input("día de la semana: ")
-dia = dia.lower()
+# Ejercicio en clase tercera semana 
+ingreso = str(input("Ingrese la fecha actual: el nombre del día, número de día y mes: "))
+ingreso = ingreso.lower()
+
+dia = ingreso[0:ingreso.find(',')]
 if(dia != "lunes" and dia != "martes" and dia != "miercoles" and dia != "jueves" and dia != "viernes"):
     dia=str("Se produjo un error.")
 
 
 
-dd = int(input("Número de la semana: "))
+dd = int(ingreso[ingreso.find(' ')+1 : ingreso.find('/')])
 if(dd > 32 or dd < 0):
     dd=str("Se produjo un error.")
 
 
-mm = int(input("Número de mes: "))
+mm = int(ingreso[ingreso.find('/')+1 : len(ingreso)])
 if(mm>12 or mm<0):
     mm=str("Se produjo un error.")
 
@@ -24,7 +25,7 @@ else:
 
 
 if(dia == "lunes" or dia == "martes" or dia == "miercoles"):
-    exa = print("Se tomaron examenes?")
+    exa = str(input("Se tomaron examenes?"))
     exa = exa.lower()
     if(exa == "si"): 
         aprob = int(input("Ingrese la cantidad de aprobados: "))
@@ -34,10 +35,10 @@ if(dia == "lunes" or dia == "martes" or dia == "miercoles"):
         desaprob1 = (desaprob*100)/tt
         if(aprob1 == 0):
             print("Desaprobó toda la clase.")
-            if(desaprob1 == 0 ):
-                print("Aprobó toda la clase.")
-            else:
-            print("Aprobó el ", aprob1, "% y desaprobó el ", desaprob1, "% de la clase")
+        if(desaprob1 == 0 ):
+            print("Aprobó toda la clase.")
+        if(aprob1 != 0 and desaprob1 != 0):
+            print("Aprobó el ", aprob1, "% y desaprobó el ", desaprob1, " % de la clase")
 
 if(dia == "jueves"):
     porcent = int(input("Ingresar el porcentaje de asistencia: "))
@@ -47,6 +48,11 @@ if(dia == "jueves"):
         print("No asistió la mayoría.")
 
 if(dia == "viernes"):
+    if((dd == 1 and mm == 1) or (dd == 1 and mm == 7)): 
+        print("Comienzo de nuevo ciclo")
+        cant = int(input("Ingrese la cantidad de alumnos: "))
+        aranceles = int(input("Ingrese el valor de arancel por alumno: "))
+        print("Con la cantidad de ", cant, " alumnos se va a obtener un ingreso total de ", aranceles*cant, "$." )
 
 
 
